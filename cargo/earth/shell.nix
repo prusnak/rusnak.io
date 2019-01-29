86c50299b75ea7b1d34242fa4e2547e2c1f8988d
Pavol Rusnak <pavol@rusnak.io>
2019-01-29 02:14:04 +0100

    nix: use python3, not python36

--- cargo/earth/shell.nix
+++ cargo/earth/shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 
 let
-  myPython = python36.withPackages(p: [p.geopy]);
+  myPython = python3.withPackages(p: [p.geopy]);
 in
   stdenv.mkDerivation {
     name = "myearth-dev";
